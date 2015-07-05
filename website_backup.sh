2bc0bed90740e2b89cc25ce876d2481d36dd23fe
Budi Prakosa a.k.a Iyok <iyok@deadmediafm.org>
2015-07-05 15:01:25 +0700

    Update website_backup.sh

--- website_backup.sh
+++ website_backup.sh
@@ -3,7 +3,7 @@
 # user, password, gzip file out
 function backup_mysql(){
 	param=" --host=127.0.0.1 -A --user='${1}' --password='${2}' --all-databases | gzip > '${3}'"
-	mysqldump ${param}
+	/usr/bin/mysqldump ${param}
 	#echo "mysqldump --host=127.0.0.1 -A --user=${bak_mysql_user} --password=${bak_mysql_pass} --all-databases | gzip > $bak_dir/$bak_mysql_file"
 }
 
