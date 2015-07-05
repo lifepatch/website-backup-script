e791d40a653ce399bf12a4ba64bfde08edb33d20
Budi Prakosa a.k.a Iyok <iyok@deadmediafm.org>
2015-07-05 13:55:17 +0700

    Update backup_now.sh

--- backup_now.sh
+++ backup_now.sh
@@ -2,12 +2,12 @@
 
 source ./website_backup.sh 
 
+bak_home=/home4/lpatch
+bak_dir=$bak_home/backup.lifepatch.org
+
 sites[0]='<folder you want to backup>,<mysql username>,<mysql password>'
 sites[1]='<folder you want to backup>,<mysql username>,<mysql password>'
 sites[2]='<folder you want to backup>,<mysql username>,<mysql password>'
-
-bak_home=/home4/lpatch
-bak_dir=$bak_home/backup.lifepatch.org
 
 #call backup function
 backup_sites
